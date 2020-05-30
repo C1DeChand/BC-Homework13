62116f2cdaf63e9197bf685a7a93a16500a10ab3
C1DeChand <C1DeChand@gmail.com>
2020-05-30 14:16:36 -0700

    localhost_working
    got localhost working, ui/buttons need major work.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('plain burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('glutin free burger', true);
+INSERT INTO burgers (burger_name) VALUES ('bacon burger');
